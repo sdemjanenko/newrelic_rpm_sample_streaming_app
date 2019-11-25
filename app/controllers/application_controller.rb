02a3e0d335dc199923774be9f5d1e3859a5ad7b3
Stephen Demjanenko <sdemjanenko@gmail.com>
2019-11-25 15:35:30 -0800

    Add DemoController and /demo route
    This route will stream a simple JSON response over 10 seconds. The
    `render_stream` helper attaches a Ruby Enumerator to the response body.
    When the body is then constructed by the server, it iterates over the
    Enumerator resulting in a chunked response. The streaming behavior can
    be seen in a browser as the JSON response slowly comes in and the
    browser displays the new data as it is received.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def render_stream(enumerator)
+    headers['Cache-Controller'] = 'no-cache'
+    headers['Last-Modified'] = Time.now.utc.httpdate
+    self.response_body = enumerator
+  end
 end
---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,27 @@
+class DemoController < ApplicationController
+  def show
+    json_enum = Enumerator.new do |yielder|
+      yielder << '['
+
+      first_element = true
+      100.times do |n|
+        yielder << ',' unless first_element
+        yielder << "{\"id\": \"#{n}\", \"value\": \"#{expensive_method_call(n)}\"}"
+        first_element = false
+      end
+
+      yielder << ']'
+    end
+
+    headers['Content-Type'] = 'application/json'
+    render_stream json_enum
+  end
+
+  private
+
+  # TODO: trace this method in Newrelic
+  def expensive_method_call(n)
+    sleep 0.1
+    "foo#{n}"
+  end
+end
